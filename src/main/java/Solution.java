89b34f12dab5179b1217f87bd4260c0a9e09b860
alek <wait82985@gmail.com>
2024-08-06 19:29:05 +0500

    problem solved agai

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,29 +1,25 @@
 import java.util.Arrays;
 
-public public class Solution {
+class Solution {
     public int minimumPushes(String word) {
-        char[] chars = word.toCharArray();
         int[] freq = new int[26];
-        for (int i = 0; i < chars.length; i++) {
-            freq[chars[i] - 'a']++;
-        }
 
+        for(int i = 0; i < word.length(); i ++) {
+            int ch = word.charAt(i) - 'a';
+            freq[ch]++;
+        }
         Arrays.sort(freq);
         int res = 0;
-        int count = 0;
-        for (int i = freq.length - 1; i >= 0; i--) {
-            if(count < 8){
-                res += freq[i];
-            } else if (count < 16) {
-                res+= freq[i] * 2;
-            }else if(count < 24){
-                res+= freq[i] * 3;
-            }else{
-                res += freq[i] * 4;
+        int c = 8;
+        int times = 1;
+        for(int i = 25; i >= 0; i--) {
+            res += freq[i] * times;
+            c--;
+            if(c == 0) {
+                c = 8;
+                times++;
             }
-            count++;
         }
         return res;
     }
 }
-
